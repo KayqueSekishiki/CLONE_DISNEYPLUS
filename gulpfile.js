54643581eb079c7cae80fbfa5829042badfbe7ee
Kayque Lopes <104032451+KayqueSekishiki@users.noreply.github.com>
2025-01-06 18:06:54 -0300

    fix: gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const sass = require("gulp-sass")(require("sass"));
 function styles() {
   return gulp
     .src("./src/styles/*.scss")
-    .pipe(sass({ outputStyles: "compressed" }))
+    .pipe(sass({ outputStyle: "compressed" }))
     .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles);
 exports.watch = function () {
-  gulp.watch("./src/styles/*.scss", gulp.parallel["styles"]);
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
 };
